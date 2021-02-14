76ac449f2b2513bb7568fd2695002698d022f080
andres.alcaraz <andres.alcaraz@numrot.com>
2021-02-13 22:18:01 -0500

    Forst commit

---
+++ src/app/components/gifs/services/gifs.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _apiKey: string = 'xTXOhxvMq9ym70HHltWoaPRu4ies4qUe';
+  private _serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _history: string[] = [];
+  public results: Gif[] = [];
+
+  get history(){
+    return [...this._history];
+  }
+
+  constructor(private http: HttpClient) { 
+    this._history = JSON.parse(localStorage.getItem('history')) || [];
+    this.results = JSON.parse(localStorage.getItem('lastSearch')) || [];
+  }
+
+  searchGifs(query: string = ''){
+    query = query.trim().toLowerCase();
+
+    if(!this._history.includes(query)){
+      this._history.unshift(query);
+      this._history = this._history.splice(0, 10);
+      
+      localStorage.setItem('history', JSON.stringify(this._history));
+    }
+
+    const params = new HttpParams()
+    .set('api_key', this._apiKey)
+    .set('limit', '10')
+    .set('q', query)
+
+    this.http.get<SearchGifsResponse>(`${this._serviceUrl}/search`, {params})
+      .subscribe((response) => {
+        this.results = response.data;
+        localStorage.setItem('lastSearch', JSON.stringify(this.results));
+      }, error => {
+        console.log(error);
+      })
+  }
+}
---
+++ src/app/components/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent  {
+
+  constructor(private gifsService: GifsService) { }
+
+  get history(){
+    return this.gifsService.history;
+  }
+
+  Search(search: string){
+    this.gifsService.searchGifs(search);
+  }
+
+}
